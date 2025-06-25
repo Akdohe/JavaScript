abec9d5a836286b2bf1687ef0e78de7914b2f34c
Akdohe <akshaydohe17@gmail.com>
2025-06-25 11:48:15 +0530

    new comit

---
+++ first.js
@@ -0,0 +1,15 @@
+//variables
+fullName = "Tony Stark";
+age = 24;
+price = 99.99;
+x = null ;
+y = undefined ;
+
+//Print value 
+console.log(fullName);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log("Welcome To Akshay World!");
+console.log(y);
+
