68ffc94568978dd121f9a83b452a58805c9eb67b
Akdohe <akshaydohe17@gmail.com>
2025-06-25 13:15:51 +0530

    New Js Codes

--- first.js
+++ first.js
@@ -1,10 +1,18 @@
 //variables
 fullName = "Tony Stark";
-age = 24;
+//age = 24;
 price = 99.99;
 x = null ;
 y = undefined ;
 
+//objcet defining //key: valuse
+const student = {
+    fullName: "Rahul Kumar",
+    age : 20,
+    cgpa: 8.2,
+    isPass : true,
+};
+console.log(student.age);
 //Print value 
 console.log(fullName);
 console.log(age);
