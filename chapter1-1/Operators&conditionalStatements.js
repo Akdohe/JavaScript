ec2b0114be47ffbfc96012b199425f5e19d1f4d7
Akdohe <akshaydohe17@gmail.com>
2025-06-27 16:16:53 +0530

    renamed the folder and uploded

---
+++ chapter1-1/Operators&conditionalStatements.js
@@ -0,0 +1,26 @@
+
+
+//objcet defining //key: valuse
+// const product = {
+//    productName : "Parker Ball pen",
+//     Rating: 8.2,
+//     offer : 5,
+//     type : "Ball Pen",
+//     price : 270,
+// };
+// console.log(product);
+// //Print value 
+// console.log(fullName);
+// console.log(age);
+
+//profile storing ot object
+const instaProfile = {
+    userName : "@AkshayDOhe",
+    isFollow: false,
+    messages : "Click here to got message",
+    following : 5,
+    folowwers: 34256,
+};
+console.log(instaProfile);
+//Print value 
+
